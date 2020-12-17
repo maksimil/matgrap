533c0ba5ecc2ccf75009df887dcbacfeac818195
Maxim Kosterov <maxim.kosterov@gmail.com>
2020-12-17 19:29:59 +0300

    Added point parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 
 pub mod canvas;
+pub mod parser;
 
 pub use canvas::*;
+pub use parser::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
---
+++ src/parser.rs
@@ -0,0 +1,40 @@
+use crate::{Canvas, Point};
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref POINT_REGEX: Regex = Regex::new(
+        "([A-Z]+[0-9]*)[[:space:]]*\\(([0-9|\\.]*)[[:space:]]*,[[:space:]]*([0-9|\\.]*)\\)"
+    )
+    .unwrap();
+}
+
+pub fn parse(text: &str) -> Canvas {
+    let mut canvas = Canvas::new();
+
+    // points
+    for cap in POINT_REGEX.captures_iter(text) {
+        println!("{:?}", cap);
+        canvas.add_point(Point {
+            label: String::from(cap.get(1).expect("Failed to get point label").as_str()),
+            x: cap
+                .get(2)
+                .map(|v| {
+                    v.as_str()
+                        .parse::<f64>()
+                        .expect("Failed to parse f64 from point position")
+                })
+                .expect("Failed to get x coord"),
+            y: cap
+                .get(3)
+                .map(|v| {
+                    v.as_str()
+                        .parse::<f64>()
+                        .expect("Failed to parse f64 from point position")
+                })
+                .expect("Failed to get y coord"),
+        });
+    }
+
+    canvas
+}
